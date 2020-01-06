78189017be678097db8d25dfa99c5278ab10abd0
yzq <739291773@qq.com>
2020-01-06 18:05:46 +0800

    使用importLoaders属性

--- webpack.config.js
+++ webpack.config.js
@@ -25,14 +25,14 @@ module.exports = {
             {
               loader: MiniCssExtractPlugin.loader
             },
-            'css-loader',
-             // 使用 postcss 为 css 加上浏览器前缀
             {
-              loader: 'postcss-loader',
+              loader: "css-loader",
               options: {
-                plugins: [require('autoprefixer')]
+                importLoaders: 2 //importLoaders: 2 表示：在一个 css 中引入了另一个 css，也会执行之前两个 loader，即 postcss-loader 和 sass-loader
               }
             },
+             // 使用 postcss 为 css 加上浏览器前缀
+            "postcss-loader",
             'sass-loader' // 使用 sass-loader 将 scss 转为 css
           ]
         }
