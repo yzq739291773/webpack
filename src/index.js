09078170c0100024c039eb72ec300cb0b3a1ffcc
yzq <739291773@qq.com>
2020-01-06 21:31:02 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// 异步代码
-import(/* webpackChunkName: 'a'*/ './a').then(function(a) {
-    console.log(a)
-  })
-  
-  import(/* webpackChunkName: 'b'*/ './b').then(function(b) {
-    console.log(b)
-  })
-  
-  import(/* webpackChunkName: 'use-lodash'*/ 'lodash').then(function(_) {
-    console.log(_.join(['1', '2']))
-  })
-  
+// app.js
+import { chunk } from 'lodash-es'
+console.log(chunk([1, 2, 3], 2))
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,31 @@
-const path = require('path');
+const path = require('path')
+
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry:{
-        main:"./src/index.js"
-    },
-    output:{
-        // js 引用路径或者cdn地址
-        publicPath:__dirname + '/dist',
-        // 打包文件的输出目录
-        path:path.resolve(__dirname,'dist'),
-        // 打包后生成的jsw文件
-        filename:"bundle.js",
-        chunkFilename: '[name].js' // 代码拆分后的文件名
-    },
-    optimization: {
-        splitChunks: {
-          chunks: 'all',
-          minSize: 10000,
-          maxSize: 0,
-          minChunks: 1,
-          maxAsyncRequests: 5,
-          maxInitialRequests: 3,
-          automaticNameDelimiter: '~',
-          name: true,
-          cacheGroups: {
-            lodash: {
-              name: 'lodash',
-              test: /[\\/]node_modules[\\/]lodash[\\/]/,
-              priority: 10  // 优先级要大于 vendors 不然会被打包进 vendors
-            },
-            commons: {
-                name: 'commons',
-                minSize: 0, //表示在压缩前的最小模块大小,默认值是 30kb
-                minChunks: 2, // 最小公用次数
-                priority: 5, // 优先级
-                reuseExistingChunk: true // 公共模块必开启
-              },
-            vendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true
-            }
-          }
-          
-        }
-      },   
-    plugins:[
-        // // 默认情况下，此插件将删除 webpack output.path目录中的所有文件，以及每次成功重建后所有未使用的 webpack 资产
-        new CleanWebpackPlugin()
-    ]
-
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    publicPath: './', // js 引用的路径或者 CDN 地址
+    path: path.resolve(__dirname, 'dist'), // 打包文件的输出目录
+    filename: '[name].bundle.js' // 代码打包后的文件名
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      // 打包输出HTML
+      title: '自动生成 HTML',
+      minify: {
+        // 压缩 HTML 文件
+        removeComments: true, // 移除 HTML 中的注释
+        collapseWhitespace: true, // 删除空白符与换行符
+        minifyCSS: true // 压缩内联 css
+      },
+      filename: 'index.html', // 生成后的文件名
+      template: 'index.html', // 根据此模版生成 HTML 文件
+      chunks: ['app'] // entry中的 app 入口才会被打包
+    })
+  ]
 }
